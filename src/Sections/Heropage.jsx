1cff24b0ccfb2a382a936d054cb7bf288e99146f
ackermanaakriti <bhandariaakriti897@gmail.com>
2024-05-20 21:04:43 +0545

    heropage changed

--- src/Sections/Heropage.jsx
+++ src/Sections/Heropage.jsx
@@ -4,33 +4,38 @@ import current from '../Global/images/redcurrent.png'
 import snicker from '../Global/images/Snickers-broken (1).png'
 import bg from '../Global/images/Rectangle 1.png'
 import Button from '../Components/Button'
+import mainimg from '../Global/images/bg66.png'
 
 
 const Heropage = () => {
   return (
     <>
       <div className='heropage' >
-        <div className='grid grid-cols-2 mx-auto container py-[30px]'>
-          <div className='px-[30px] '>
-            <figure className=' heropageimg'>
-              <img className='h-[100px] img1' src={digestive} />
-              <img className='h-[150px] img2' src={current} />
-              <img className='h-[100px] img3' src={snicker}/>
-            </figure>
-          </div>
-          <div>
-            <div className='pt-[100px]'>
-              <p className='font-poppins text-[20px]  text-secondarycolor'>Abhiyan Sales and Marketing</p>
-              <h2 className='text-[60px] font-kurale pt-[20px] font-[600] text-secondarycolor'>Market your products
+        <div className='grid grid-cols-2 '>
+          <div className='px-[30px]  mx-auto container '>
+          <div className='pt-[100px]'>
+              <p className='font-poppins text-[20px]  text-primarycolor'>Abhiyan Sales and Marketing</p>
+              <h2 className='text-[60px] font-kurale pt-[20px] font-[600] text-primarycolor'>Market your products
 Anywhere & Anytime</h2>
                <div className='pt-[30px]'>
-             <Button bg='secondarycolor' textclr='primarycolor' text='Read More'/>
-              </div>
+               <Button bg='primarycolor' txtclr='secondarycolor' textsize='14' text='Read More'/>              </div>
               <p>
 
               </p>
-            </div>
+            {/* <figure className=' heropageimg'>
+              <img className='h-[100px] img1' src={digestive} />
+              <img className='h-[150px] img2' src={current} />
+              <img className='h-[100px] img3' src={snicker}/>
+            </figure> */}
+             </div>
           </div>
+          <div className='left--section col-span-1'>
+            <figure className='h-[430px] flex justify-center items-center absolute right-[60px]  top-[20%]'>
+              <img className='h-full' src={mainimg}/>
+            </figure>
+          
+            </div>
+         
         </div>
      
       </div>
